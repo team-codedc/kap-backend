49195cc5222f6dae8d241dc98bcb770ae95a9c22
rlaeodnr1011 <skfvkfl1011@gmail.com>
2022-08-05 12:19:19 +0900

    feat: db 도커 환경 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env' : '.env.local',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production').required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mariadb',
+          host: configService.get<string>('TYPEORM_HOST'),
+          port: parseInt(configService.get<string>('TYPEORM_PORT'), 10),
+          username: configService.get<string>('TYPEORM_USERNAME'),
+          password: configService.get<string>('TYPEORM_PASSWORD'),
+          database: configService.get<string>('TYPEORM_DATABASE'),
+          entities: [path.join(__dirname, configService.get<string>('TYPEORM_ENTITIES'))],
+          logging: configService.get<string>('TYPEORM_LOGGING') === 'true',
+          synchronize: configService.get<string>('NODE_ENV') === 'development',
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
