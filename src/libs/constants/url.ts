60c4fd1236ff86d2c96a28a53944178b220d9478
rlaeodnr1011 <skfvkfl1011@gmail.com>
2022-08-14 08:33:38 +0900

    refactor: 사용하지 않는 상수 제거

--- src/libs/constants/url.ts
+++ src/libs/constants/url.ts
@@ -1,7 +1,5 @@
-const KAKAO_TOKEN_URL = 'https://kauth.kakao.com/oauth/token';
 const KAKAO_USER_INFO_URL = 'https://kapi.kakao.com/v2/user/me';
 
-const GOOGLE_TOKEN_URL = 'https://www.googleapis.com/oauth2/v4/token';
 const GOOGLE_USER_INFO_URL = 'https://www.googleapis.com/oauth2/v2/userinfo';
 
-export { KAKAO_TOKEN_URL, KAKAO_USER_INFO_URL, GOOGLE_TOKEN_URL, GOOGLE_USER_INFO_URL };
+export { KAKAO_USER_INFO_URL, GOOGLE_USER_INFO_URL };
