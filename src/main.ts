468e5f6026f4621791fe9a20e579acdfcb702e52
HanSeo0507 <i.hansu.lee@gmail.com>
2022-08-28 01:07:05 +0900

    feat: morgan 미들웨어 추가

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './libs/interceptors';
 import { HttpExceptionFilter } from './libs/filters';
 import * as cookieParser from 'cookie-parser';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
+  app.use(morgan('common'));
   app.use(cookieParser());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
